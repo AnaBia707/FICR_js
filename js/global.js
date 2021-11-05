5e357ca0cb873089241e51c0e37b5fac5dcf5237
ANA BEATRIZ DA SILVA ALBUQUERQUE <ana.balbuquerque@a.ficr.edu.br>
2021-11-04 22:07:48 -0300

    arquivo aula2

---
+++ js/global.js
@@ -0,0 +1,9 @@
+const cardNovaLista = document.getElementById('cardNovaLista')
+const btnNovaLista = document.getElementById('btnNovaLista')
+
+btnNovaLista.addEventListener('click', function() {
+    cardNovaLista.style.display = 'block'
+})
+cancel.addEventListener('click', function() {
+    cancel.style.display = 'none'
+})
